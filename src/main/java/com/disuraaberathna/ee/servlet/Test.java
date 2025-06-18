7fbfc6edb66928dca485f43f4aa63b9f792bf1c8
Disura Aberathna <disura2005@gmail.com>
2025-06-18 11:01:49 +0530

    Create Test.java

---
+++ src/main/java/com/disuraaberathna/ee/servlet/Test.java
@@ -0,0 +1,22 @@
+package com.disuraaberathna.ee.servlet;
+
+import com.disuraaberathna.ee.ejb.UserSessionBean;
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/test")
+public class Test extends HttpServlet {
+    @EJB
+    private UserSessionBean userSessionBean;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        userSessionBean.doAction();
+    }
+}
