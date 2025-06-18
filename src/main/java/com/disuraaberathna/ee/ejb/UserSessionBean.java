26292a69d9b07ca4c3fd600595b9ea6f2d21ad5f
Disura Aberathna <disura2005@gmail.com>
2025-06-18 13:47:23 +0530

    Update UserSessionBean.java

--- src/main/java/com/disuraaberathna/ee/ejb/UserSessionBean.java
+++ src/main/java/com/disuraaberathna/ee/ejb/UserSessionBean.java
@@ -1,13 +1,14 @@
 package com.disuraaberathna.ee.ejb;
 
+import com.disuraaberathna.ee.interceptor.AInterceptor;
 import com.disuraaberathna.ee.interceptor.TestInterceptor;
 import jakarta.ejb.Stateless;
 import jakarta.interceptor.Interceptors;
 
 @Stateless
-@Interceptors(TestInterceptor.class)
+@Interceptors({TestInterceptor.class, AInterceptor.class})
 public class UserSessionBean {
-    public void doAction(){
-        System.out.println("UserSessionBean - doAction");
+    public void doAction(String name, int age) {
+        System.out.println("UserSessionBean - doAction : " + name + ", " + age);
     }
 }
