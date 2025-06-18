b4c6b0967edfce5c664c6d5843fb5cefde0727e6
Disura Aberathna <disura2005@gmail.com>
2025-06-18 13:47:18 +0530

    Update TestInterceptor.java

--- src/main/java/com/disuraaberathna/ee/interceptor/TestInterceptor.java
+++ src/main/java/com/disuraaberathna/ee/interceptor/TestInterceptor.java
@@ -1,12 +1,63 @@
 package com.disuraaberathna.ee.interceptor;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.interceptor.AroundConstruct;
 import jakarta.interceptor.AroundInvoke;
 import jakarta.interceptor.InvocationContext;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Map;
+
 public class TestInterceptor {
+
+    @AroundConstruct
+    public void aroundConstruct(InvocationContext ctx){
+        System.out.println("TestInterceptor - aroundConstruct : " + ctx.getConstructor());
+    }
+
+    @PostConstruct
+    public void init(InvocationContext ctx) {
+        Constructor<?> constructor=ctx.getConstructor();
+
+        System.out.println("TestInterceptor init");
+    }
+
     @AroundInvoke
     public Object intercept(InvocationContext ctx) throws Exception {
         System.out.println("TestInterceptor - intercept");
-        return ctx.proceed();
+
+//        Method method = ctx.getMethod();
+//        System.out.println("TestInterceptor - method" + method);
+
+//        Object[] parameters = ctx.getParameters();
+//        Arrays.stream(parameters).forEach(System.out::println);
+
+//        ctx.setParameters(new Object[]{"A", 50});
+
+//        Object target = ctx.getTarget();
+//        System.out.println("Target : " + target);
+
+//        Map<String, Object> map = ctx.getContextData();
+//        System.out.println("contextData: " + map);
+//        map.put("name", "ABC");
+
+        // InvocationContext used for single scope like as a one context for a one single method call
+        // InvocationContext works in a local scope
+
+        // Can not inject InvocationContext
+
+        Constructor<?> constructor = ctx.getConstructor();
+        System.out.println(constructor);
+
+        ctx.proceed();
+        return null;
+    }
+
+    @PreDestroy
+    public void destroy(InvocationContext ctx) {
+        System.out.println("TestInterceptor - destroy");
     }
 }
