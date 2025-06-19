4999f7e358bcba3dfcd83c69b5faa6064ed88a16
Disura Aberathna <disura2005@gmail.com>
2025-06-19 10:08:56 +0530

    Update AInterceptor.java

--- src/main/java/com/disuraaberathna/ee/interceptor/AInterceptor.java
+++ src/main/java/com/disuraaberathna/ee/interceptor/AInterceptor.java
@@ -8,10 +8,14 @@ import java.util.Map;
 public class AInterceptor {
     @AroundInvoke
     public Object aroundInvoke(InvocationContext ctx) throws Exception {
-        System.out.println("AInterceptor - intercept : " + ctx.getMethod().getName());
-        Map<String, Object> map = ctx.getContextData();
-        System.out.println("context data : " + map);
-        ctx.proceed();
-        return null;
+//        System.out.println("AInterceptor - intercept : " + ctx.getMethod().getName());
+        System.out.println("AInterceptor - intercept : start");
+//        Map<String, Object> map = ctx.getContextData();
+//        System.out.println("context data : " + map);
+        Object proceed = ctx.proceed();
+        System.out.println("AInterceptor - intercept : proceed / " + proceed);
+
+        System.out.println("AInterceptor - intercept : end");
+        return "A";
     }
 }
