b54452c71548f5f65f79c997d68ad1b6df28db77
Disura Aberathna <disura2005@gmail.com>
2025-06-19 11:16:42 +0530

    Update LogginInterceptor.java

--- src/main/java/com/disuraaberathna/ee/interceptor/LogginInterceptor.java
+++ src/main/java/com/disuraaberathna/ee/interceptor/LogginInterceptor.java
@@ -1,12 +1,14 @@
 package com.disuraaberathna.ee.interceptor;
 
 import com.disuraaberathna.ee.annotation.Loggin;
+import jakarta.annotation.Priority;
 import jakarta.interceptor.AroundInvoke;
 import jakarta.interceptor.Interceptor;
 import jakarta.interceptor.InvocationContext;
 
 @Interceptor
 @Loggin
+@Priority(1)
 public class LogginInterceptor {
     @AroundInvoke
     public Object intercept(InvocationContext ic) throws Exception {
