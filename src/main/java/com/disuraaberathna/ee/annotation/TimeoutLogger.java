50e56e0f9e956dd3bbdeca270523318d210efbe9
Disura Aberathna <disura2005@gmail.com>
2025-06-19 11:16:29 +0530

    Create TimeoutLogger.java

---
+++ src/main/java/com/disuraaberathna/ee/annotation/TimeoutLogger.java
@@ -0,0 +1,12 @@
+package com.disuraaberathna.ee.annotation;
+
+import jakarta.interceptor.InterceptorBinding;
+
+import java.lang.annotation.*;
+
+@Inherited
+@InterceptorBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface TimeoutLogger {
+}
