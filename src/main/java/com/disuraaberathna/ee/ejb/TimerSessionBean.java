ea74f91dd34ff03215b156f7a4250552dc44320a
Disura Aberathna <disura2005@gmail.com>
2025-06-19 12:16:10 +0530

    Update TimerSessionBean.java

--- src/main/java/com/disuraaberathna/ee/ejb/TimerSessionBean.java
+++ src/main/java/com/disuraaberathna/ee/ejb/TimerSessionBean.java
@@ -1,11 +1,15 @@
 package com.disuraaberathna.ee.ejb;
 
+import com.disuraaberathna.ee.annotation.TimeoutLogger;
 import jakarta.ejb.Schedule;
 import jakarta.ejb.Stateless;
+import jakarta.ejb.Timeout;
 
 @Stateless
+@TimeoutLogger
 public class TimerSessionBean {
     @Schedule(hour = "*", minute = "*", second = "*/10", persistent = false)
+    @Timeout
     public void doAction() {
         System.out.println("TimerSessionBean doAction");
     }
